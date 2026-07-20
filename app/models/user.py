0e5ee3100da0acfb245413111d2971d235105994
SyedFaizFarrukh <faizfrrk@gmail.com>
2026-07-20 17:15:58 +0500

    WIP: user authentication setup

--- app/models/user.py
+++ app/models/user.py
@@ -2,7 +2,9 @@ from datetime import datetime
 from sqlalchemy import String, Integer, DateTime 
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from app.database import Base
-from app.models.project import Project
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from app.models.project import Project
 
 class User(Base):
     __tablename__ = "users"
